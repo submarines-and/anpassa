74e4af236f8b8b3ddb04370e352cea72a2c4ae3e
Johannes Sörensen <johannes.sorensen@ingka.ikea.com>
2021-03-28 11:22:21 +0200

    Fixed paths in script, because it tried to write at root

--- backup.sh
+++ backup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Create data dir if missing
-mkdir -p ./data
+mkdir -p ~/dev/anpassa/data
 
-cp ~/.config/karabiner/karabiner.json ./data/karabiner.json
-cp ~/Library/Developer/Xcode/UserData/KeyBindings/Default.idekeybindings  ./data/xcode-keybindings.idekeybindings
+cp ~/.config/karabiner/karabiner.json ~/dev/anpassa/data/karabiner.json
+cp ~/Library/Developer/Xcode/UserData/KeyBindings/Default.idekeybindings  ~/dev/anpassa/data/xcode-keybindings.idekeybindings
