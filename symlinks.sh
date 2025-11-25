af367ec7246170dfa2a54504829b9ee9c69bc5c1
submarines-and <cached_lifters2j@icloud.com>
2025-11-25 06:57:57 +0100

    Fixed symlink to karabiner config (folder instead of file)

--- symlinks.sh
+++ symlinks.sh
@@ -2,7 +2,7 @@
 
 
 
-ln -s ~/dev/anpassa/data/karabiner.json ~/.config/karabiner/karabiner.json 
+ln -s ~/dev/anpassa/data/karabiner ~/.config/karabiner 
 ln -s ~/dev/anpassa/data/xcode-keybindings.idekeybindings ~/Library/Developer/Xcode/UserData/KeyBindings/Default.idekeybindings  
 
 
