ed20119d48e9968d7c3385df47acfc3cca582998
submarines-and <cached_lifters2j@icloud.com>
2025-11-25 06:57:57 +0100

    Switched to standard vs code editor due to bugs in insider

--- symlinks.sh
+++ symlinks.sh
@@ -6,5 +6,5 @@ ln -s ~/dev/anpassa/data/karabiner.json ~/.config/karabiner/karabiner.json
 ln -s ~/dev/anpassa/data/xcode-keybindings.idekeybindings ~/Library/Developer/Xcode/UserData/KeyBindings/Default.idekeybindings  
 
 
-ln -s ~/dev/anpassa/data/vscode-settings.json ~/Library/Application\ Support/Code\ -\ Insiders/User/settings.json 
-ln -s ~/dev/anpassa/data/vscode-keybindings.json ~/Library/Application\ Support/Code\ -\ Insiders/User/keybindings.json 
+ln -s ~/dev/anpassa/data/vscode-settings.json ~/Library/Application\ Support/Code/User/settings.json 
+ln -s ~/dev/anpassa/data/vscode-keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json 
