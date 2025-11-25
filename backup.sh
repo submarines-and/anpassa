dcd427214021c64b06c3f216eb244a25fd1ec385
submarines-and <cached_lifters2j@icloud.com>
2025-11-25 06:57:56 +0100

    Added vscode-files

--- backup.sh
+++ backup.sh
@@ -5,3 +5,5 @@ mkdir -p ~/dev/anpassa/data
 
 cp ~/.config/karabiner/karabiner.json ~/dev/anpassa/data/karabiner.json
 cp ~/Library/Developer/Xcode/UserData/KeyBindings/Default.idekeybindings  ~/dev/anpassa/data/xcode-keybindings.idekeybindings
+cp ~/Library/Application\ Support/Code\ -\ Insiders/User/settings.json ~/dev/anpassa/data/vscode-settings.json
+cp ~/Library/Application\ Support/Code\ -\ Insiders/User/keybindings.json ~/dev/anpassa/data/vscode-keybindings.json
